e64a4420fcb648d4a6aaf1168f12cf5c015c1006
paragbhtngr <paragbhtngr@gmail.com>
2016-11-10 16:09:01 +0000

    pushing to digital ocean server

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000, function(){
-  console.log('App started on port 3000');
+app.listen(8080, function(){
+  console.log('App started on port 8080');
 });
 
 
